44ceca0777fd60501843652f567b52bc134997b2
Sean Liew <seanliew@google.com>
2020-05-21 21:23:29 -0400

    Rename project

--- src/main/java/DdexMeadParser.java
+++ src/main/java/DdexMeadParser.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class XSDProto {
+public class DdexMeadParser {
     public static void main(String args[]) throws IOException {
         System.out.println("Launched XSD parse tester");
 
