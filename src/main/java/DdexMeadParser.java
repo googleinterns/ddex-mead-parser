f5e5ab94c47ebda25acfe31c6aa5d7a8cac5334d
Sean Liew <s3liew@uwaterloo.ca>
2020-06-09 16:27:04 -0400

    Fixed structure for building in intellij testing

--- src/main/java/DdexMeadParser.java
+++ src/main/java/DdexMeadParser.java
@@ -19,7 +19,7 @@ public class DdexMeadParser {
   public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, TransformerException, Descriptors.DescriptorValidationException {
     // Build custom object representation of XSD (Xml schema) to be used by the ProtoDescriptorBuilder to create the file descriptor
     ProtoSchemaBuilder protoBuilder = new ProtoSchemaBuilder(/* Options from cmd line */ );
-    protoBuilder.ingestXsdFromPath("src/prototype/resources/ern42/release-notification.xsd");
+    protoBuilder.ingestXsdFromPath("src/main/resources/ern42/release-notification.xsd");
     EntryContainer entryContainer = protoBuilder.parseXsd();
 
     // Create protobuf FileDescriptor for the schema
@@ -32,14 +32,14 @@ public class DdexMeadParser {
 
     // Write an (automatically) merge-able version of the message XML
     XmlFixer xmlFixer = new XmlFixer(fileDescriptor);
-    String rootName = xmlFixer.fixFromPath("src/prototype/resources/3 MixedMedia.xml");
+    String rootName = xmlFixer.fixFromPath("src/main/resources/3 MixedMedia.xml");
 
     // Get builder for message being parsed | TODO Add null check
     Descriptors.Descriptor rootMessage = fileDescriptor.findMessageTypeByName(rootName);
     DynamicMessage.Builder builder = DynamicMessage.newBuilder(rootMessage);
 
     // Merge transformed XML with generated builder
-    File initialFile = new File("src/prototype/resources/3 MixedMedia.xmlr");
+    File initialFile = new File("src/main/resources/3 MixedMedia.xmlr");
     InputStream asXml = new FileInputStream(initialFile);
     XmlFormat xmlFormat = new XmlFormat();
     xmlFormat.merge(asXml, builder);
